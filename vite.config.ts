d6c210f1deaf5a1cfb8a550dccc4d0bd9b3ea122
Nicolas Jara Carvajal <nicolas.jaracarvajal@utem.cl>
2025-09-07 23:41:31 -0300

    Se realiza cambio y se agrega netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import netlify from '@netlify/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Iterum/',
-  plugins: [react()],
+  base: '/iterum/',
+  plugins: [react(), netlify()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
